53d2a834e386e7b88b3865245d6ca12db427b247
mehdielaoufir <elaoufirmehdi5@gmail.com>
2025-09-16 12:35:49 -0400

    .net maui addition

---
+++ MauiApp.Thera/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MauiApp.Thera;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ MauiApp.Thera/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace MauiApp.Thera;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
