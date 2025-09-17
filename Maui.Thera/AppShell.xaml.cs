71e74c2461e15c3179138067ea28f4835b03bd4b
mehdielaoufir <elaoufirmehdi5@gmail.com>
2025-09-17 14:44:56 -0400

    new maui

---
+++ Maui.Thera/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Maui.Thera;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ Maui.Thera/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "Maui.Thera")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "Maui.Thera.Pages")]
