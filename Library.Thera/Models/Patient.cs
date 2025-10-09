3a5e96aabc1ece722f4571ffbb9c1defd00c7ef7
mehdielaoufir <elaoufirmehdi5@gmail.com>
2025-10-09 08:55:33 -0400

    update to run only mac catalyst

--- Library.Thera/Models/Patient.cs
+++ Library.Thera/Models/Patient.cs
@@ -2,11 +2,18 @@ namespace CLI.Thera.Models
 {
     public class Patient
     {
+        public int Id { get; set; }
+
         public string? Name { get; set; }
         public string? Address { get; set; }
+
         public DateTime BirthDate { get; set; }
+
         public string? Race { get; set; }
         public string? Gender { get; set; }
+
+        public string? Diagnoses { get; set; }
+        public string? Prescriptions { get; set; }
 
         public override string ToString()
         {
